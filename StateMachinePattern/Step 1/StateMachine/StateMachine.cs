0fe1889cd6712da7af5f003180b3a936348234d5
zimmybln <zimmy@web.de>
2017-05-01 20:40:50 +0200

    A first checkin and project Step 1 setup

---
+++ StateMachinePattern/Step 1/StateMachine/StateMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    public class StateMachine
+    {
+        private readonly List<State> _states = new List<State>();
+
+
+        public List<State> States
+        {
+            get { return _states; }
+        }
+    }
+
+    public static class ListOfStateExtender
+    {
+        public static bool Contains(this List<State> list, string name)
+        {
+            return list.Any(s => s.Name.Equals(name));
+        }
+    }
+
+
+    public class State
+    {
+        public State(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public static implicit operator State(string name)
+        {
+            return new State(name);
+        }
+    }
+}
---
+++ StateMachinePattern/Step 1/StateMachine/StateMachineBaseTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StateMachine
+{
+    [TestFixture]
+    public class StateMachineBaseTests
+    {
+        [Test]
+        public void AddAState()
+        {
+            const string statename = "a simple state";
+
+            var statemachine = new StateMachine();
+
+            statemachine.States.Add(new State(statename));
+
+            Assert.IsTrue(statemachine.States.Count == 1);
+            Assert.IsTrue(statemachine.States.Any(s => s.Name.Equals(statename)));
+        }
+    }
+}
