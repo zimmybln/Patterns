33233830362da5364884fdf5f222eef4da87e581
Torsten Zimmermann <zimmy@web.de>
2020-09-04 10:29:46 +0200

    Test für Records C# 9

--- State/StateWithLogistics/Components/ITransportContext.cs
+++ State/StateWithLogistics/Components/ITransportContext.cs
@@ -9,3 +9,4 @@ namespace StateWithLogistics.Components
         int Weight { get; }
     }
 }
+ 
---
+++ State/StateWithLogistics/ImmutableState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace StateWithLogistics
+{
+    public record CustomState
+    {
+        public int FirstValue { get; set; }
+        public string Name { get; set; }
+    }
+
+    [TestFixture]
+    public class ImmutableState
+    {
+        [Test]
+        public void CreateChangedStateCopy()
+        {
+            CustomState state = new CustomState() { FirstValue = 20, Name = "Paul" };
+
+            var state2 = state with { FirstValue = 30 };
+
+            Assert.AreEqual(20, state.FirstValue);
+            Assert.AreEqual("Paul", state2.Name);
+        }
+    }
+}
